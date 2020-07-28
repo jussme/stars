891957d3d4464e5566df46a4223232d5db090cab
jussme <marcin.kapusek@gmail.com>
2020-07-28 23:33:57 +0200

    formula copied from a yt video - works wonderfully

--- stars/src/base/Galaxy.java
+++ stars/src/base/Galaxy.java
@@ -10,15 +10,18 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Galaxy extends JFrame{
 	private static final long serialVersionUID = 1L;
+	static final short REFRESH_DELAY = 2;
 	final short WIDTH;
 	final short HEIGHT;
-	static final short REFRESH_DELAY = 32;
 	final short NAUGHT_TO_CORNER;
 	
 	public static void main(String[] args) {
@@ -32,17 +35,15 @@ public class Galaxy extends JFrame{
 		
 		WIDTH = (short) screenSize.width;
 		HEIGHT = (short) screenSize.height;
-		NAUGHT_TO_CORNER = (short) (sqrt(pow((WIDTH/2), 2) + pow((HEIGHT/2), 2)) * 1.5);
-		
+		NAUGHT_TO_CORNER = (short) (sqrt(pow((WIDTH/2), 2) + pow((HEIGHT/2), 2)));
+
 		setLayout(new GridLayout(1,1));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		
-		Star.setGalaxy(this);
-		
 		class GalaxyWindow extends JPanel{
 			private static final long serialVersionUID = 1L;
-			private final static short STAR_COUNT = 25;
+			private final static short STAR_COUNT = 1;
 			final Star[] STARS = new Star[STAR_COUNT];
 			
 			@Override
@@ -51,15 +52,49 @@ public class Galaxy extends JFrame{
 				Graphics2D g2 = (Graphics2D) g;
 				g2.setColor(Color.WHITE);
 				g2.translate(Galaxy.this.WIDTH/2, Galaxy.this.HEIGHT/2);
-				g2.setStroke(new BasicStroke(0.25f));
+				g2.setStroke(new BasicStroke(0.1f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
 				for(int it = 0; it < STAR_COUNT; ++it) {
 					STARS[it].paintStar_Trail(g2);
 				}
+				g2.drawOval(0, 0, 3, 3);
 			}
 			
 			GalaxyWindow(){
+				addMouseListener(new MouseListener() {
+
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						for(Star star : STARS)
+							star.recycle();
+					}
+
+					@Override
+					public void mousePressed(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void mouseReleased(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void mouseEntered(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void mouseExited(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+				});
 				for ( int it = 0; it < STAR_COUNT; ++it)
-					STARS[it] = new Star();
+					STARS[it] = new Star(Galaxy.this);
 				
 				setBackground(Color.BLACK);
 				
@@ -69,11 +104,13 @@ public class Galaxy extends JFrame{
 						{
 							STARS[it].update();
 						}
+						
 						try {
 							Thread.sleep(REFRESH_DELAY);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
+						
 						this.repaint();
 					}
 				}).start();
--- stars/src/base/Star.java
+++ stars/src/base/Star.java
@@ -4,47 +4,76 @@ import java.awt.Graphics2D;
 import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 import static java.lang.Math.random;
-public class Star {
-	static Galaxy GALAXY;
-	private final static short STAR_SIZE = 10;
-	private final static double OFFSET_Z_CALC  = 0.4;
-	private final static double OFFSET_COORD_INCREASE = 0.1;
-	private short sx;
-	private short sy;
+
+/*public class Star {
+	private final Galaxy parentGalaxy;
+	private final static double OFFSET_MIN_VALUE  = 0.5;
+	private final static int MULTIPLIER = 2;
+	private final double halfDiagonalMultiplied;
+	private double sx;
+	private double sy;
 	private double x;
 	private double y;
 	private double z;
 	
-	static void setGalaxy(Galaxy galaxy){
-		GALAXY = galaxy;
-	}
-	
-	Star(){
-		sx = (short) (random() * GALAXY.WIDTH - GALAXY.WIDTH/2);
-		sy = (short) (random() * GALAXY.HEIGHT - GALAXY.HEIGHT/2);
-		x = sx;
-		y = sy;
-		z = sqrt(pow(x, 2) + pow(y, 2))/GALAXY.NAUGHT_TO_CORNER + OFFSET_Z_CALC;
+	Star(Galaxy galaxy){
+		parentGalaxy = galaxy;
+		halfDiagonalMultiplied = parentGalaxy.NAUGHT_TO_CORNER * MULTIPLIER;
+		recycle();
+		z = sqrt(pow(x, 2) + pow(y, 2))/halfDiagonalMultiplied + OFFSET_MIN_VALUE;
 	}
 	
 	public void paintStar_Trail(Graphics2D g) {
-		short radius = (short) (STAR_SIZE * z);
-		g.fillOval((int)(x-radius/2), (int)(y-radius/2), radius, radius);
-		g.drawLine((int)x, (int)y, sx, sy);
+		//short radius = (short) (12 * z);
+		//g.fillOval((int)(x-radius/2), (int)(y-radius/2), radius, radius);
+		g.drawLine((int)x, (int)y, (int)sx, (int)sy);
 	}
 	
 	void update() {
-		x *= 1 + z/8; //+ OFFSET_COORD_INCREASE;
-		y *= 1 + z/8; //+ OFFSET_COORD_INCREASE;
-		z = (sqrt(pow(x, 2) + pow(y, 2))/GALAXY.NAUGHT_TO_CORNER) + OFFSET_Z_CALC;
+		x *= 1 + z/16;
+		y *= 1 + z/16;
+		z = (sqrt(pow(x, 2) + pow(y, 2))/halfDiagonalMultiplied) + OFFSET_MIN_VALUE;
 		if(z >= 1)
 			recycle();
 	}
 	
 	void recycle() {
-		sx = (short) (random() * GALAXY.WIDTH - GALAXY.WIDTH/2);
-		sy = (short) (random() * GALAXY.HEIGHT - GALAXY.HEIGHT/2);
+		double width = parentGalaxy.WIDTH , height = parentGalaxy.HEIGHT ;
+		sx = (short) (random() * width - width/2);
+		sy = (short) (random() * width - height/2);
 		x = sx;
 		y = sy;
+	}
+}*/
+public class Star {
+	private final Galaxy parentGalaxy;
+	private double sx;
+	private double sy;
+	private double z;
+	
+	Star(Galaxy galaxy){
+		parentGalaxy = galaxy;
+		
+		recycle();
+	}
+	
+	public void paintStar_Trail(Graphics2D g) {
+		float galaxyWidthByDepth = (float) (parentGalaxy.HEIGHT/z);
+		int x = (int) (galaxyWidthByDepth * sx);
+		int y = (int) (galaxyWidthByDepth * sy);
+		short radius = (short) (3 * galaxyWidthByDepth);
+		g.fillOval((int)(x-radius/2), (int)(y-radius/2), radius, radius);
+		//g.drawLine( x, y, (int) sx, (int) sy);
+	}
+	
+	void update() {
+		if(--z < 0)
+			recycle();
+	}
+	
+	void recycle() {
+		sx = (short) (random() * parentGalaxy.WIDTH - parentGalaxy.WIDTH/2);
+		sy = (short) (random() * parentGalaxy.HEIGHT - parentGalaxy.HEIGHT/2);
+		z = random() * (parentGalaxy.HEIGHT - 1) + 1;
 	}
 }
